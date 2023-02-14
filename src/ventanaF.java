b8673bcac139005e7ca0947ed296285ecc8fa0c1
Wilson <wilson.guayanay@epn.edu.ec>
2023-02-14 10:36:42 -0500

    Initial commit

---
+++ src/ventanaF.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+public class ventanaF {
+    private JLabel JFarmacia;
+    private JLabel JCedula;
+    private JLabel JCiudad;
+    private JTextField txtCedula;
+    private JTextField txtCantidad;
+    private JComboBox comboBoxCiudad;
+    private JTextField txtNombreC;
+    private JLabel JNombre;
+    private JLabel JProducto;
+    private JComboBox comboBoxProducto;
+    private JLabel JDireccion;
+    private JTextField txtDireccion;
+    private JLabel JWilson;
+    private JButton botonBuscar;
+    private JButton botonBorrar;
+    private JButton botonActualizar;
+    private JButton botonCrear;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
